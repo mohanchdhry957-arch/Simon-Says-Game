9adbb38335802c8c21ed22fda9b98e84e0f5e89a
Mohan Choudhary <mohanchdhry957@gmail.com>
2026-07-31 21:15:06 +0530

    First commit - Simon Says Game

---
+++ app.js
@@ -0,0 +1,77 @@
+let gameSeq = [];
+let userSeq = [];
+let btns = ["yellow", "red", "purple", "green"];
+
+let started = false;
+let level = 0;
+
+let h3 = document.querySelector("h3");
+
+document.addEventListener("keypress", function () {
+    if(started == false) {
+        console.log("game started");
+        started = true;
+        levelUp();
+    }
+})
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove("flash")
+    }, 150);
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h3.innerText = "Level " + level;
+
+    let randIdx = Math.floor(Math.random() * 4);
+    let randColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor); 
+    btnFlash(randbtn);
+    console.log(randColor);
+    console.log(randbtn);
+
+    
+}
+
+function checkAns(idx) {
+
+    if(userSeq[idx] === gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h3.innerText = `Game over! press any key to start score was ${level}`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
+}
+
+function btnPress() {
+    let btn = this;
+    btnFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length - 1);
+}
+
+let allbtns = document.querySelectorAll(".btn");
+for(btn of allbtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
